975ffeb42c7cb5e2e4876cb9f99e1424c351118f
dharshib <Dharshib.8@gmail.com>
2024-03-25 19:17:05 +0530

    add app.js

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
+require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const userController = require('./controllers/userController')
+const authRoutes = require('./routes/authRouter')
+const userRoutes = require('./routes/userRoutes')
+const tagRoutes = require('./routes/tagRoutes')
+const blogRoutes = require('./routes/blogRoutes')
+const likeRoutes = require('./routes/likeRoutes')
+const commentRoutes = require('./routes/commentRouter')
+const connectToDB = require('./config/dbConnection')
 
-const port = 9000;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/first')
-    .then(() => {
-        console.log("Connected to MongoDB");
-        app.listen(port, () => {
-            console.log('Application is running on port ' + port)
-        })
-    })
-    .catch(
-        (error) =>  {
-            console.error("Error connecting to MongoDB:", error);
-        }
-    );
+connectToDB()
 
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
+app.use('/tags', tagRoutes)
+app.use('/blogs', blogRoutes)
+app.use('/likes', likeRoutes)
+app.use('/comments', commentRoutes)
 
-app.get('/users', userController.getAllUsers);
-
-app.post('/new', userController.addUser)
-
-app.put('/update/:id', userController.updateUser)
-
-app.delete('/delete/:id', userController.deleteUser)
-
-app.get('/user/:id', userController.getUserById);
-
-
+app.listen(port, () => {
+    console.log('Application is running on port ' + port)
+})
